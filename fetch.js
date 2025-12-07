f43e3db08814a0697492c4dffe2498bfb3b4c2fe
sagorsenanupam <senanupam10@gmail.com>
2025-12-07 11:32:49 +0600

    edited

--- fetch.js
+++ fetch.js
@@ -1,3 +1,5 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+const HandOnData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json))
+}
